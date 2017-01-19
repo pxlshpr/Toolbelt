dda2955f5777932392899e77e84f0cca2344e59a
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-19 18:09:15 +0800

    Make the isValidHexadecimal function public

--- Toolbelt/String.swift
+++ Toolbelt/String.swift
@@ -6,7 +6,7 @@ public extension String {
      A Boolean value indicating whether a string only contains hexadecimal characters.
      */
 
-    var isValidHexadecimal: Bool {
+    public var isValidHexadecimal: Bool {
         let chars = CharacterSet(charactersIn: "0123456789ABCDEF").inverted
         guard self.rangeOfCharacter(from: chars, options: .caseInsensitive, range: nil) == nil else {
             return false
