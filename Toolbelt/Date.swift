f8dcbf2221c8ebd8312458859dd21a7d9af26399
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-21 20:22:50 +0800

    move date extension to String file for now to trigger code coverage data (as it's not in the Xcode project right now), also remove extraneous comments and add documentation for firstWord

--- Toolbelt/Date.swift
+++
@@ -1,5 +0,0 @@
-public extension Date {
-    public var startOfDay: Date {
-        return Calendar.current.startOfDay(for: self)
-    }
-}
--- Toolbelt/String.swift
+++ Toolbelt/String.swift
@@ -1,24 +1,26 @@
 public extension String {
 
+    /**
+     A string value that returns the first word of this string, if any. Any leading whitespaces or newlines are ignored.
+     */
     public var firstWord: String? {
         return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: " ").first
     }
     
-    
-    //TODO: Maybe make this a variable instead? So change wherever it's used. Also get back that bash function 'ft' or whatever that finds the text you're looking for in the directory you're in and all its files/subdirectories
-     //TODO: write documentation for these functions, and all others in Toolbelt. Have a workflow in mind for doing it regularly
-     //TODO: Convert all code in all project's to two spaces being a tab (as per what Chris Lattner said)
-     //TODO: Write tests *after* checking out what the code coverage badge is like without any for what we just added
-     //TODO: Don't forget to replace the code we brough in (firstWord and the Date extension) with the framework in other projects 
     /**
      A Boolean value indicating whether a string only contains hexadecimal characters.
      */
-
     public var isValidHexadecimal: Bool {
         let chars = CharacterSet(charactersIn: "0123456789ABCDEF").inverted
         guard self.rangeOfCharacter(from: chars, options: .caseInsensitive, range: nil) == nil else {
             return false
         }
         return true
+    }
+}
+
+public extension Date {
+    public var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
     }
 }
