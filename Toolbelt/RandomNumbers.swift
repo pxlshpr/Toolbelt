fe3acefbf1b7e6a633c70305dbcde5294ac51b81
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-23 09:16:12 +0800

    Add basic random number function with tests

---
+++ Toolbelt/RandomNumbers.swift
@@ -0,0 +1,9 @@
+//
+//  RandomNumbers.swift
+//  Toolbelt
+//
+//  Created by Ahmed Khalaf on 23/1/17.
+//  Copyright © 2017 Ahmed Khalaf. All rights reserved.
+//
+
+import Foundation
--- ToolbeltTests/RandomNumbersTests.swift
+++ ToolbeltTests/RandomNumbersTests.swift
@@ -11,7 +11,24 @@ class RandomNumbersTests: XCTestCase {
     super.tearDown()
   }
   
-  func testRandomNumberWithLowerLimitOf0() {
+  func testRandomNumbersFrom0() {
+    let upperLimits = [0, 5, 100, ]
+    for upperLimit in upperLimits {
+      let random = randomIntegerBetween0(and: upperLimit)
+      XCTAssertGreaterThanOrEqual(random, 0, "Random number generated was less than 0")
+      XCTAssertGreaterThanOrEqual(upperLimit, random, "Random number generated was greater than the upper limit")
+    }
+  }
+  
+  func testRandomNumberInARange() {
+    let ranges = [(0, 10), (100, 1000), (9, 10), (0, 0)]
+    for range in ranges {
+      for _ in 0..<5 {
+        let random = randomIntegerInclusively(between: range.0, and: range.1)
+        XCTAssertLessThanOrEqual(range.0, random, "Random number was less than the lower limit")
+        XCTAssertGreaterThanOrEqual(range.1, random, "Random number was greater than the upper limit")
+      }
+    }
   }
   
 }
