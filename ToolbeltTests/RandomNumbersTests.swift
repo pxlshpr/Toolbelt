c385059294350e1f6edacf25aad13582112dfd9a
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-28 07:28:53 +0800

    create sample data from different types of Ints and UInts

--- ToolbeltTests/RandomNumbersTests.swift
+++ ToolbeltTests/RandomNumbersTests.swift
@@ -21,32 +21,39 @@ class RandomNumbersTests: XCTestCase {
   
   func testRandomNumbersFrom0() {
     
-    let limitsInt = [-5000000, -5000, -100, -5, -2, -1, 0, 1, 2, 5, 100, 5000]
+    let limitsInt = [Int.min, -5000000000000, -5000000, -5000, -100, -5, -2, -1, 0, 1, 2, 5, 100, 5000, 5000000, 5000000000000, Int.max]
+    let limitsUInt: [UInt] = [0, 1, 2, 5, 100, 5000, 5000000, 5000000000000, UInt.max]
+
     var limitsInt8: [Int8] = []
     var limitsInt16: [Int16] = []
     var limitsInt32: [Int32] = []
     var limitsInt64: [Int64] = []
-    var limitsUInt: [UInt] = []
+
     var limitsUInt8: [UInt8] = []
     var limitsUInt16: [UInt16] = []
     var limitsUInt32: [UInt32] = []
     var limitsUInt64: [UInt64] = []
- 
-    for  limit in limitsInt {
-      //TODO: handle negatives with unsigned types else overflows!
-      limitsInt8.append(Int8(max(Int8.min, min(limit, Int8.max))))
-      limitsInt16.append(Int16(max(Int16.min, min(limit, Int16.max))))
-      limitsInt32.append(Int32(max(Int32.min, min(limit, Int32.max))))
-      limitsInt64.append(Int64(max(Int64.min, min(limit, Int64.max))))
-      limitsUInt.append(UInt(max(UInt.min, min(limit, UInt.max))))
-      limitsUInt8.append(UInt8(max(UInt8.min, min(limit, UInt8.max))))
-      limitsUInt16.append(UInt16(max(UInt16.min, min(limit, UInt16.max))))
-      limitsUInt32.append(UInt32(max(UInt32.min, min(limit, UInt32.max))))
-      limitsUInt64.append(UInt64(max(UInt64.min, min(limit, UInt64.max))))
+
+    for limit in limitsInt {
+      limitsInt8.append(Int8(max(Int(Int8.min), min(limit, Int(Int8.max)))))
+      limitsInt16.append(Int16(max(Int(Int16.min), min(limit, Int(Int16.max)))))
+      limitsInt32.append(Int32(max(Int(Int32.min), min(limit, Int(Int32.max)))))
+      limitsInt64.append(Int64(max(Int(Int64.min), min(limit, Int(Int64.max)))))
+    }
+
+    for limit in limitsUInt {
+      limitsUInt8.append(UInt8(max(UInt(UInt8.min), min(limit, UInt(UInt8.max)))))
+      limitsUInt16.append(UInt16(max(UInt(UInt16.min), min(limit, UInt(UInt16.max)))))
+      limitsUInt32.append(UInt32(max(UInt(UInt32.min), min(limit, UInt(UInt32.max)))))
+      limitsUInt64.append(UInt64(max(UInt(UInt64.min), min(limit, UInt(UInt64.max)))))
+    }
+
+    var limits: [Any] = []
+    let arrays = [limitsInt, limitsInt8, limitsInt16, limitsInt32, limitsInt64, limitsUInt, limitsUInt8, limitsUInt16, limitsUInt32, limitsUInt64] as [[Any]]
+    for array in arrays {
+      limits.append(contentsOf: array)
     }
     
-    //TODO: find out how to 'swift define array with generic'.
-    // then add all randoms here as we generate them (and make sure we have a lot more samples to test on).
     // then assert (in the end) that all the generated randoms weren't:
     // 1. the same (but only depending on the range. So a range of (0, 0) if applicable might be an exception)
     // 2. too similar to each other (ie. too many repeats – but that depends on the range doens't it? don't divulge in this too much. Maybe just trust that even a slight randomness (ie. different number) is random enough
