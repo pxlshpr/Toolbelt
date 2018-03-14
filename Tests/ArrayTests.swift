84d0486e89beaba5b31ee53f5283b1b668363dc6
Ahmed Khalaf <pxlshpr@gmail.com>
2018-03-14 23:18:46 +0800

    organize project structure

--- Tests/ArrayTests.swift
+++ Tests/ArrayTests.swift
@@ -14,6 +14,8 @@ class ArrayTests: XCTestCase {
   func testAllEqual() {
     XCTAssertTrue([1, 1, 1].containsDuplicates)
     XCTAssertFalse([1, 2, 1].containsDuplicates)
+    XCTAssertFalse([1, 2, 3].containsDuplicates)
+    //TODO: change this name to contains the same element only because 1,2,1 techincally has '1' duplicated
     //TODO: make this a property, in the Swift 3 spirit
     //TODO: write more obscure test cases to try and break this
   }
