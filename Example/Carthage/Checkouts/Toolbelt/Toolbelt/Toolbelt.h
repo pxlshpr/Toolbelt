06d829c9b9b57e74c6ba9d2a3430522f291cc7b4
Ahmed Khalaf <pxlshpr@gmail.com>
2018-04-16 11:10:10 +0800

    remove test images and update gitignore

--- Example/Carthage/Checkouts/Toolbelt/Toolbelt/Toolbelt.h
+++
@@ -1,8 +0,0 @@
-//#import <UIKit/UIKit.h>
-@import Foundation;
-
-//! Project version number for Toolbelt.
-FOUNDATION_EXPORT double ToolbeltVersionNumber;
-
-//! Project version string for Toolbelt.
-FOUNDATION_EXPORT const unsigned char ToolbeltVersionString[];
